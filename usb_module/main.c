f9de8fcda79c255e407139981a3b3aacaed433f4
Siarhei Klimuts <galiaf47@gmail.com>
2014-08-30 13:07:53 +0300

    Added Imperial March on 30th status (build failed) Firmware size optimizations and API statuses changed for reducing firmware logic

--- usb_module/main.c
+++ usb_module/main.c
@@ -6,12 +6,22 @@
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 #include <avr/wdt.h>
+#include <util/delay.h>
 
 #include "usbdrv/usbconfig.h"
 #include "usbdrv/usbdrv.h"
 
-#include <util/delay.h>
+#define NOTES_COUNT 39
+#define SERIALIZE_DEVIDER 10
+#define PORT_D_THRESHOLD 40
+#define PLAY_ON_STATUS 30
+#define FREQUENCY_DEV 5000
+#define DURATION_MULTIPLIER 15
+#define TICK_DELAY 100 //us
+#define FREQUECY_EDGE 3
+#define BLINK_DELAY 2000
 
 PROGMEM const char usbHidReportDescriptor[22] = { // USB report descriptor
     0x06, 0x00, 0xff,                       // USAGE_PAGE (Generic Desktop)
@@ -26,13 +36,41 @@ PROGMEM const char usbHidReportDescriptor[22] = { // USB report descriptor
     0xc0                                    // END_COLLECTION
 };
 
-uint8_t status = 1;
+PROGMEM const uint16_t frequences[NOTES_COUNT] = {
+  392, 392, 392, 311, 466, 392, 311, 466, 392,
+  587, 587, 587, 622, 466, 369, 311, 466, 392,
+  784, 392, 392, 784, 739, 698, 659, 622, 659,
+  415, 554, 523, 493, 466, 440, 466,
+  311, 369, 311, 466, 392 };
+
+PROGMEM const uint16_t durations[NOTES_COUNT] = {
+  350, 350, 350, 250, 100, 350, 250, 100, 700,
+  350, 350, 350, 250, 100, 350, 250, 100, 700,
+  350, 250, 100, 350, 250, 100, 100, 100, 450,
+  150, 350, 250, 100, 100, 100, 450,
+  150, 350, 250, 100, 750 };
+
+volatile uint8_t *blinkPort = &PORTB;
+uint8_t pin;
+uint8_t isBlink;
+uint8_t play;
 
 USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
 	usbRequest_t *rq = (void *)data; // cast data to correct type
-	status = rq->wValue.bytes[0];
+	uint8_t status = rq->wValue.bytes[0]; //40/41 - 20/21 - 30/31
     PORTD &=~ ((1 << PD5) | (1 << PD4));
     PORTB &=~ ((1 << PD2) | (1 << PD3));
+
+	if(status < PORT_D_THRESHOLD) {
+		blinkPort = &PORTB;
+	} else {
+		blinkPort = &PORTD;
+	}
+
+	isBlink = status % SERIALIZE_DEVIDER;
+	pin = status / SERIALIZE_DEVIDER;
+	play = status % PLAY_ON_STATUS == 0;
+
 	return 0;
 }
 
@@ -53,77 +91,51 @@ int main(void) {
 
 	sei();
 
-	int16_t delay = 0;
-	uint8_t pin = PD5;
-	uint8_t isBlink = 0;
+	int16_t blinkDelay = 0;
+	uint16_t soundDelay = 0;
+	uint8_t note = 0;
+	uint16_t duration = 0;
+	uint16_t frequency = 0;
 
 	while(1) {
 		wdt_reset();
 		usbPoll();
 
-		switch (status) {
-			case 1:
-				pin = PD5;
-				isBlink = 0;
-				break;
-			case 2:
-				pin = PD5;
-				isBlink = 1;
-				break;
-			case 3:
-				pin = PD4;
-				isBlink = 0;
-				break;
-			case 4:
-				pin = PD4;
-				isBlink = 1;
-				break;
-			case 5:
-				pin = PB2;
-				isBlink = 0;
-				break;
-			case 6:
-				pin = PB2;
-				isBlink = 1;
-				break;
-			case 7:
-				pin = PB3;
-				isBlink = 0;
-				break;
-			case 8:
-				pin = PB3;
-				isBlink = 1;
-				break;
+		blinkDelay++;
+		if(blinkDelay > BLINK_DELAY) {
+			blinkDelay = -BLINK_DELAY;
 		}
 
 		if(isBlink == 0) {
-			if(status < 5) {
-				PORTD |= (1 << pin);
-			} else {
-				PORTB |= (1 << pin);
-			}
+			*blinkPort |= (1 << pin);
 		} else {
-			delay++;
-			if(delay > 200) {
-				delay = -200;
-			}
-
-			if(delay < 0) {
-				if(status < 5) {
-					PORTD |= (1 << pin);
-				} else {
-					PORTB |= (1 << pin);
-				}
-			} else {
-				if(status < 5) {
-					PORTD &=~ (1 << pin);
-				} else {
-					PORTB &=~ (1 << pin);
-				}
+			if(blinkDelay == 0) {
+				*blinkPort ^= (1 << pin);
 			}
 		}
 
-		_delay_ms(2);
+		if(play == 1) {
+			if(soundDelay < duration) {
+				if(soundDelay % frequency <= FREQUECY_EDGE) {
+					PORTD ^= (1 << PD5);
+				}
+			} else {
+				soundDelay = 0;
+				if(note == NOTES_COUNT) {
+					note = 0;
+					play = 0;
+				}
+
+				duration = pgm_read_word_near(durations + note) * DURATION_MULTIPLIER;
+				frequency = FREQUENCY_DEV / pgm_read_word_near(frequences + note);
+
+				note++;
+			}
+
+			soundDelay++;
+		}
+
+		_delay_us(TICK_DELAY);
 	}
 
 	return 0;
