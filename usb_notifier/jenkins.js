1de38493d4f5a3715a08f846e74af144a3a27b91
Siarhei Klimuts <galiaf47@gmail.com>
2014-09-03 18:03:46 +0300

    Still doesn't work after USB unplug

--- usb_notifier/jenkins.js
+++ usb_notifier/jenkins.js
@@ -8,23 +8,33 @@ var FAIL_STATUS = 'FAILURE';
 var JENKINS_URL = process.env.JENKINS_URL;
 
 var jenkins = jenkinsapi.init(JENKINS_URL);
-function cb(error, data) {
 
-		if(error) {
-			console.error('USB transfer:', error);
-		}
-}
 exports.checkStatus = function checkStatus(){
 	jenkins.last_build_info(JOB_NAME, function(err, data){
+		var usbStatus;
 		var now = new Date();
  		console.log('Staus = ', data.result, now.toLocaleTimeString());
  		
- 	 	if(data.result == SUCCESS_STATUS){
- 	 		usbConnector.sendStatus(usbConnector.SUCCESS_STATUS, cb);
- 	 	}else if(data.result == PROGRESS_STATUS){
- 	 		usbConnector.sendStatus(usbConnector.PROGRESS_STATUS, cb);
- 	 	}else if(data.result == FAIL_STATUS){
- 	 		usbConnector.sendStatus(usbConnector.FAIL_STATUS, cb);
- 	 	}
+ 		usbStatus = data.result == SUCCESS_STATUS ? usbConnector.SUCCESS_STATUS
+ 		 			: data.result == PROGRESS_STATUS ? usbConnector.PROGRESS_STATUS
+ 		 			: data.result == FAIL_STATUS ? usbConnector.FAIL_STATUS
+ 		 			: 1;
+ 	 		
+ 	 	usbConnector.sendStatus(usbStatus, function (error, data) {
+ 	 		if(!error) {
+ 	 			
+ 	 		} else {
+				console.error('USB transfer:', error);
+				usbConnector.connect();
+			}
+ 	 	});
+
+ 	 	// if(data.result == SUCCESS_STATUS){
+ 	 	// 	usbConnector.sendStatus(usbConnector.SUCCESS_STATUS, cb);
+ 	 	// }else if(data.result == PROGRESS_STATUS){
+ 	 	// 	usbConnector.sendStatus(usbConnector.PROGRESS_STATUS, cb);
+ 	 	// }else if(data.result == FAIL_STATUS){
+ 	 	// 	usbConnector.sendStatus(usbConnector.FAIL_STATUS, cb);
+ 	 	// }
 	});
 }
--- usb_notifier/usbConnector.js
+++ usb_notifier/usbConnector.js
@@ -1,25 +1,26 @@
 var usb = require('usb');
+var device;
 
 exports.FAIL_STATUS = 30;
 exports.PROGRESS_STATUS = 21;
 exports.SUCCESS_STATUS = 40;
 
-exports.sendStatus = function(status, done){
-	var interf;
-	var device;
-
+function connect() {
+	// device && device.close();
 	device = usb.findByIds(5824, 1500);
+	device && device.open();
+}
 
+exports.connect = connect;
+
+exports.sendStatus = function(status, done){
 	if(device) {
-		device.open();
-		// interf = device.interface(0);
-		// interf.claim();	
-		
 		device.controlTransfer(usb.LIBUSB_REQUEST_TYPE_RESERVED, usb.LIBUSB_TRANSFER_TYPE_CONTROL, status, 0, new Buffer(0), function(error, data) {
 			done(error, data);
-			device.close();
 		});
 	} else {
 		done(new Error('Device is not found.'));
 	}
 }
+
+connect();
